d4e09c23b636fae705a2deeb794ef8e0dcc2b89d
Rafał Borowiak <rborowiak@euvic.pl>
2018-01-29 19:12:03 +0100

    Added day of week to filters table.

--- scripts/app/controller/filterTable.controller.js
+++ scripts/app/controller/filterTable.controller.js
@@ -11,13 +11,23 @@ angular.module('betterTimetable')
         }
 
         var _weeks = {
-            'EvenWeek': "Parzysty", 
+            'EvenWeek': "Parzysty",
             'OddWeek': "Nieparzysty"
+        }
+
+        var _days = {
+            'MONDAY': "Poniedziałek",
+            'TUESDAY': "Wtorek",
+            'WEDNESDAY': "Środa",
+            'THURSDAY': "Czwartek",
+            'FRIDAY': "Piątek",
+            'SATURDAY': "Sobota",
+            'SUNDAY': "Niedziela"
         }
 
         var _getLabelColor = function(filter){
             return UISrv.getColor(filter);
-        } 
+        }
 
         $scope.getColor = function(id) {
             var filter =  $scope.filterColors.find(function (obj) { return obj.filterId === id; });
@@ -27,17 +37,18 @@ angular.module('betterTimetable')
         var _getFilters = function () {
 
             FilterRsc.getFilters(function (data) {
-            
+
             $scope.filters = [];
-            $scope.filterColors = []; 
+            $scope.filterColors = [];
 
                 data.forEach(function (filter) {
 
                     $scope.filters.push({
                         courseType: _labels[filter.courseType],
-                        fullCourseName: filter.fullCourseName, 
-                        id: filter.id, 
-                        week: _weeks[filter.week], 
+                        fullCourseName: filter.fullCourseName,
+                        id: filter.id,
+                        week: _weeks[filter.week],
+                        dayOfWeek: _days[filter.dayOfWeek],
                         hours: _getHours(filter.time, filter.duration)
                     });
 
@@ -50,11 +61,11 @@ angular.module('betterTimetable')
             }, function () {
                 Materialize.toast('We couldn\'t load your selected filters. Please try again', 4000);
             });
-        }   
+        }
 
         $scope.remove = function (filterId) {
             FilterRsc.removeFilter({id: filterId}, function () {
-                _getFilters(); 
+                _getFilters();
                 Materialize.toast('Success', 4000);
             }, function () {
                 Materialize.toast('Error. Please try again', 8000);
@@ -63,17 +74,17 @@ angular.module('betterTimetable')
 
         var _getHours = function(begin, duration){
             if(begin !== null && duration !== null) {
-                
+
                 var beginCourseDate = new Date(1970, 0, 1);
-                beginCourseDate.setHours(begin.hour + 1); 
-                beginCourseDate.setMinutes(begin.minute); 
-    
+                beginCourseDate.setHours(begin.hour + 1);
+                beginCourseDate.setMinutes(begin.minute);
+
                 var startTxt = beginCourseDate.getHours() + ":" + (beginCourseDate.getMinutes() === 0 ? "00" : beginCourseDate.getMinutes());
-                
+
                 var end = new Date(beginCourseDate);
                 end.setTime(beginCourseDate.getTime() + (duration.seconds * 1000));
                 var endTxt = end.getHours() + ":" + (end.getMinutes() === 0 ? "00" : end.getMinutes());
-    
+
                 return startTxt + " - " + endTxt;
             }
         }
