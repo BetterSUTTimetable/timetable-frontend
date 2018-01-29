4ed630927032d4baeb02551d4c8bd52fad2fcb54
Rafał Borowiak <rborowiak@euvic.pl>
2018-01-29 16:51:09 +0100

    Fixed tree view controller bug.

--- scripts/app/controller/treeView.controller.js
+++ scripts/app/controller/treeView.controller.js
@@ -2,30 +2,30 @@
 angular.module('betterTimetable')
 .controller('TreeViewCtrl', function ($scope, RootCategoriesRsc, SubcategoriesRsc) {
 
-    $scope.root; 
+    $scope.root;
 
     RootCategoriesRsc.query().$promise
         .then(function(root) {
             $scope.root = root;
         })
         .then(() => {
-            
+
             for (var i = 0; i < $scope.root.length; i++) {
-            
-                var idRootCategory = $scope.root[i]['id']; 
-                $scope.root[i].collapsed = true; 
-                query(idRootCategory, $scope.root); 
+
+                var idRootCategory = $scope.root[i]['id'];
+                $scope.root[i].collapsed = true;
+                query(idRootCategory, $scope.root);
             }
         })
-        .catch(console.error); 
-    
-        selectedNode(); 
+        .catch(console.error);
+
+        selectedNode();
 
 
     function selectedNode() {
         $scope.$watch( 'timetable.currentNode', function( newObj, oldObj ) {
             if( $scope.timetable && angular.isObject($scope.timetable.currentNode) ) {
-                getSelectedNodeId($scope.timetable.currentNode.id); 
+                getSelectedNodeId($scope.timetable.currentNode.id);
             }
         }, false);
     }
@@ -33,28 +33,27 @@ angular.module('betterTimetable')
     function getSelectedNodeId(id)
     {
         console.log( 'Node Selected!!' );
-        console.log("id of node" + id); 
+        console.log("id of node" + id);
     }
 
 
     function query(id, tree){
-    
+
         SubcategoriesRsc.query({id: id}).$promise
-        .then(function(result) {  
+        .then(function(result) {
+          console.log("Chilren of " + id);
+          console.log(result);
 
             if(result.length > 0){
-                let position = findPositionOfId(id, tree); 
+                let position = findPositionOfId(id, tree);
 
                 if(position !== -1)
                 {
-                    tree[position].collapsed = true; 
+                    tree[position].collapsed = true;
                     tree[position].children = result;
 
                     for (let i = 0; i < result.length; i++){
-                         
-                        for (let i = 0; i < result.length; i++){
-                            return query(result[i]['id'], tree[i].children); 
-                        }                  
+                        query(result[i]['id'], tree[position].children);
                     }
                 }
             }
